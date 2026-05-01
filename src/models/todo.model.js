41d024d3dbe1188c00ab518fd3142cb5ea2177c1
Moqtadir <abdulmoqtadir1@gmail.com>
2026-05-01 19:56:57 +0530

    create todo model

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -16,14 +16,43 @@ import mongoose from "mongoose";
  */
 
 const todoSchema = new mongoose.Schema(
-  {
-    // Your schema fields here
-  },
-  {
-    // Schema options here
-  }
+    {
+        // Your schema fields here
+        title: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 150,
+            required: [true, "Title is Required"],
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium",
+        },
+        tags: {
+            type: [String],
+            default: [],
+            validate: {
+                validator: function (v) {
+                    return v.length <= 10;
+                },
+                message: "You cannot have more than 10 tags",
+            },
+        },
+    },
+    {
+        // Schema options here
+        timestamps: true,
+    },
 );
 
 // TODO: Add index
+todoSchema.index({ completed: 1, createdAt: -1 });
 
 // TODO: Create and export the Todo model
+export const Todo = mongoose.model("Todo", todoSchema);
